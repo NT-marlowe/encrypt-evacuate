4d16b301070f16c22c7b1dd6a3ad245ef54edc59
NT-marlowe <n.tezuka196.ninj@gmail.com>
2024-07-25 14:53:32 +0900

    specify x86_64 as target arch, worked.

--- capture_ssl.c
+++ capture_ssl.c
@@ -2,13 +2,30 @@
 
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include <linux/ptrace.h>
+
+#define MAX_DATA_LEN 4096
+
+struct ssl_data_event {
+	__u64 timestamp_ns;
+	__u32 pid;
+	__u32 tid;
+	char data[MAX_DATA_LEN];
+	int data_len;
+};
 
 SEC("uprobe/lib/x86_64-linux-gnu/libssl.so")
 // SEC("uprobe/usr/lib/x86_64-linux-gnu/libssl.so")
 // SEC("uprobe/usr/lib/python3.10/lib-dynload/"
 // "_ssl.cpython-310-x86_64-linux-gnu.so")
 int probe_entry_SSL_write(struct pt_regs *ctx) {
-	bpf_printk("Entry point of SSL_write\n");
+	__u64 current_pid_gid = bpf_get_current_pid_tgid();
+	__u32 pid             = current_pid_gid >> 32;
+
+	const char *buf = (const char *)PT_REGS_PARM2(ctx);
+
+	bpf_printk("Entry point of SSL_write, buf = %p\n", buf);
 
 	return 0;
 }
