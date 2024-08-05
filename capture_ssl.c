e13f7ba57f94c9f6a6b9a915d2d024d8be4ed9a6
NT-marlowe <n.tezuka196.ninj@gmail.com>
2024-08-05 21:00:53 +0900

    lookup element (WIP)

--- capture_ssl.c
+++ capture_ssl.c
@@ -7,6 +7,7 @@
 
 #define MAX_DATA_LEN 4096
 #define MAX_STACK_DEPTH 127
+#define TASK_COMM_LEN 16
 
 struct enc_data_event_t {
 	__u64 timestamp_ns;
@@ -34,11 +35,21 @@ int BPF_PROG(fentry_read, const unsigned int fd, const char *buf) {
 	if (fd < 0) {
 		return 0;
 	}
-	bpf_printk("read: %d\n", fd);
+
+	char comm[16] = {0};
+	bpf_get_current_comm(&comm, sizeof(comm));
+	// ToDo: filter with pid
+	if (comm[0] != 'e') {
+		return 0;
+	}
+
+	bpf_printk("read: %d, buf: %p\n", fd, buf);
 	bpf_map_update_elem(&ptr_to_fd, &buf, &fd, BPF_ANY);
 	return 0;
 }
 
+// int EVP_EncryptUpdate(EVP_CIPHER_CTX *ctx, unsigned char *out,
+//   int *outl, const unsigned char *in, int inl);
 SEC("uprobe/lib/x86_64-linux-gnu/"
 	"libcrypto.so.3:EVP_"
 	"EncryptUpdate")
@@ -50,23 +61,31 @@ int probe_entry_EVP_EncryptUpdate(struct pt_regs *ctx) {
 		return 0;
 	}
 
-	__u64 current_pid_tgid = bpf_get_current_pid_tgid();
+	const char *plaintext_buf = (const char *)PT_REGS_PARM4(ctx);
+
+	// pointer of plaintext should be stored in ptr_to_fd
+	bpf_printk("encrypt: %p\n", plaintext_buf);
+	if (plaintext_buf == NULL ||
+		bpf_map_lookup_elem(&ptr_to_fd, &plaintext_buf) == NULL) {
+		bpf_printk("no fd\n");
+		return 0;
+	}
+
 	struct enc_data_event_t *event;
 	event = bpf_ringbuf_reserve(&events_ringbuf, sizeof(*event), 0);
 	if (event == NULL) {
 		return 0;
 	}
 
-	// int EVP_EncryptUpdate(EVP_CIPHER_CTX *ctx, unsigned char *out,
-	//   int *outl, const unsigned char *in, int inl);
-	const char *plaintext_buf = (const char *)PT_REGS_PARM4(ctx);
-	const int len             = PT_REGS_PARM5(ctx);
+	const int len = PT_REGS_PARM5(ctx);
 	event->data_len =
 		(len < MAX_DATA_LEN ? (len & (MAX_DATA_LEN - 1)) : MAX_DATA_LEN);
 
 	bpf_probe_read_user(event->data, event->data_len, plaintext_buf);
-	event->pid = current_pid_tgid >> 32;
-	event->tid = current_pid_tgid;
+
+	__u64 current_pid_tgid = bpf_get_current_pid_tgid();
+	event->pid             = current_pid_tgid >> 32;
+	event->tid             = current_pid_tgid;
 
 	bpf_ringbuf_submit(event, 0);
 
