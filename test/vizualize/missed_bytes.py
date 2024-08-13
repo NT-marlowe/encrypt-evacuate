6d3787e14e87bcf202dd476ce7fe8864514a6120
NT-marlowe <n.tezuka196.ninj@gmail.com>
2024-08-13 18:39:26 +0900

    heat map?

--- test/vizualize/missed_bytes.py
+++ test/vizualize/missed_bytes.py
@@ -1,4 +1,6 @@
 import sys
+import matplotlib.pyplot as plt
+import numpy as np
 
 BYTES_PER_VALUE = 4
 
@@ -15,7 +17,24 @@ def find_missing_bytes(recovered_file_path: str, byte_size: int) -> list[bool]:
         return result
 
 
+def viz_boolean_array(arr: list[bool]):
+    length = len(arr)
+    tate_length = 1000
+    yoko_length = length // tate_length
+    arr_2d = np.array(arr).reshape(tate_length, yoko_length)
+    plt.imshow(arr_2d, cmap="gray_r", aspect="auto")
+
+    plt.xticks(
+        ticks=np.arange(0, yoko_length, 500),
+        labels=np.arange(1, yoko_length + 1, 500),
+    )
+    plt.yticks(ticks=np.arange(0, 1000, 100), labels=np.arange(0, 1000, 100))
+
+    # plt.title("Captured Bytes")
+    plt.savefig("captured_bytes.png")
+
+
 recovered_file_path = sys.argv[1]
 byte_size = int(sys.argv[2])
-result = find_missing_bytes(recovered_file_path, byte_size)
-print(f"count of True: {sum(result)}")
+
+viz_boolean_array(find_missing_bytes(recovered_file_path, byte_size))
