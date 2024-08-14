6ce6b030e89cd7e569c774c928cc9b2777f4fe00
NT-marlowe <n.tezuka196.ninj@gmail.com>
2024-08-14 11:36:18 +0900

    result ok

--- test/measure_elapsed_time.sh
+++ test/measure_elapsed_time.sh
@@ -8,36 +8,47 @@ fi
 
 set -u
 set -e
-set -x
+# set -x
 
-../ebpf-ssl tmp &
-ebpf_pid=$!
+function run_measure() {
+    ../ebpf-ssl tmp &
+    ebpf_pid=$!
 
-echo "ebpf start"
+    echo "ebpf start"
 
-sleep 1
+    sleep 1
 
-data_filename=$1
-base_filename=$(basename $data_filename .data)
-echo $base_filename
-cat /sys/kernel/debug/tracing/trace_pipe \
-    > ./result/elapsed_time_${base_filename}.txt 2>/dev/null &
-cat_pid=$!
+    data_filename=$1
+    base_filename=$(basename $data_filename .data)
+    cat /sys/kernel/debug/tracing/trace_pipe \
+        > ./result/elapsed_time_${base_filename}.txt 2>/dev/null &
+    cat_pid=$!
 
-echo "Start to measure elapsed time: ${cat_pid}"
+    echo "Start to measure elapsed time: ${cat_pid}"
 
-sleep 1
+    sleep 1
 
-for i in {1..10}; do
-    ./my_simple_ransomware $data_filename -e
+    for i in {1..10}; do
+        ./my_simple_ransomware $data_filename -e
+        sleep 0.5
+    done
+
+    kill $cat_pid
+
+    sleep 0.5
+
+    kill -SIGINT $ebpf_pid
+
+    sleep 1
+
+    rm /usr/tmp/data_shelter/tmp
+    ps auxww | grep ebpf 
+}
+
+for file in $(ls ./data | grep -v enc | egrep '^0'); do
+    echo "Start to measure elapsed time: $file"
+    run_measure ./data/$file
+    
+    echo "------------"
 done
 
-kill $cat_pid
-
-sleep 0.5
-
-kill -SIGINT $ebpf_pid
-
-sleep 1
-
-rm /usr/tmp/data_shelter/tmp
