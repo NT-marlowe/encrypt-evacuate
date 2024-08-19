62b72c3deca82a7f2775565635ad62412b66ce64
NT-marlowe <n.tezuka196.ninj@gmail.com>
2024-08-19 21:57:28 +0900

    refactor

--- test/calc_all.sh
+++ test/calc_all.sh
@@ -9,5 +9,5 @@ subcom=$1
 power_2=(2 4 8 16 32)
 for i in ${power_2[@]}; do
     echo "Running with ${i}MiB ring buffer"
-    ./calc_match.sh ${subcom} $i > ./result/buf_capability/${subcom}_${i}MiB.csv
+    ./calc_reten_match.sh ${subcom} $i > ./result/buf_capability/${subcom}_${i}MiB.csv
 done
