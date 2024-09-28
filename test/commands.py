01b769068cab7628a06748be6fb07295bb8462b7
NT-marlowe <n.tezuka196.ninj@gmail.com>
2024-09-28 18:37:09 +0900

    rename

--- test/commands.py
+++ test/commands.py
@@ -19,22 +19,22 @@ if __name__ == "__main__":
         sys.exit(0)
 
     original_file_path = sys.argv[2]
-    recovered_file_path = sys.argv[3]
+    sheltered_file_path = sys.argv[3]
 
     if subcommand == "dist":
-        # recovery_rate = calculate_match_rate(original_file_path, recovered_file_path)
+        # recovery_rate = calculate_match_rate(original_file_path, sheltered_file_path)
         recovery_rate, partial_ratio = calculate_dist_and_ratio(
-            original_file_path, recovered_file_path
+            original_file_path, sheltered_file_path
         )
         print(f"{recovery_rate:.3f}, {partial_ratio}")
 
     elif subcommand == "match":
-        recovery_rate = calculate_match_rate(original_file_path, recovered_file_path)
+        recovery_rate = calculate_match_rate(original_file_path, sheltered_file_path)
         print(f"{recovery_rate:.3f}")
 
     elif subcommand == "reten":
         retention_rate = calculate_retention_rate(
-            original_file_path, recovered_file_path
+            original_file_path, sheltered_file_path
         )
         print(f"{retention_rate:.3f}")
 
--- test/subcommands/calc.py
+++ test/subcommands/calc.py
@@ -1,46 +1,45 @@
-import Levenshtein
 from rapidfuzz import fuzz
 import os
 
 
-def calculate_match_rate(original_file_path, recovered_file_path):
+def calculate_match_rate(original_file_path, sheltered_file_path):
     with open(original_file_path, "rb") as f:
         original_content = f.read()
 
-    with open(recovered_file_path, "rb") as f:
-        recovered_content = f.read()
+    with open(sheltered_file_path, "rb") as f:
+        sheltered_content = f.read()
 
     # Ensure both files are of the same length for comparison
-    length = min(len(original_content), len(recovered_content))
+    length = min(len(original_content), len(sheltered_content))
     match_count = sum(
-        1 for i in range(length) if original_content[i] == recovered_content[i]
+        1 for i in range(length) if original_content[i] == sheltered_content[i]
     )
 
     # Calculate recovery rate
-    recovery_rate = match_count / len(original_content)
-    return recovery_rate
+    match_rate = match_count / len(original_content)
+    return match_rate
 
 
-def calculate_dist_and_ratio(original_file_path, recovered_file_path):
-    with open(original_file_path, "r") as f:
-        original_content = f.read()
+# def calculate_dist_and_ratio(original_file_path, sheltered_file_path):
+#     with open(original_file_path, "r") as f:
+#         original_content = f.read()
 
-    with open(recovered_file_path, "r") as f:
-        recovered_content = f.read()
+#     with open(sheltered_file_path, "r") as f:
+#         recovered_content = f.read()
 
-    # Calculate Levenshtein distance
-    distance = Levenshtein.distance(original_content, recovered_content)
+#     # Calculate Levenshtein distance
+#     distance = Levenshtein.distance(original_content, recovered_content)
 
-    # Calculate the similarity
-    similarity = 1 - (distance / max(len(original_content), len(recovered_content)))
+#     # Calculate the similarity
+#     similarity = 1 - (distance / max(len(original_content), len(recovered_content)))
 
-    # Calculate recovery rate as a percentage
-    recovery_rate = similarity
-    partial_ratio = fuzz.partial_ratio(original_content, recovered_content)
-    return recovery_rate, partial_ratio
+#     # Calculate recovery rate as a percentage
+#     recovery_rate = similarity
+#     partial_ratio = fuzz.partial_ratio(original_content, recovered_content)
+#     return recovery_rate, partial_ratio
 
 
-def calculate_retention_rate(original_file_path, recovered_file_path):
+def calculate_retention_rate(original_file_path, sheltered_file_path):
     original_size = os.path.getsize(original_file_path)
-    recovered_size = os.path.getsize(recovered_file_path)
+    recovered_size = os.path.getsize(sheltered_file_path)
     return recovered_size / original_size
