4043eb73e3e8e2102a52447d977ab35e8e0f7ce6
NT-marlowe <n.tezuka196.ninj@gmail.com>
2024-10-05 16:04:47 +0900

    make type

--- types.go
+++ types.go
@@ -4,17 +4,28 @@ import (
 	"github.com/cilium/ebpf/ringbuf"
 )
 
+const (
+	DataBlockSize uint = 4096
+)
+
 type indexedRecord struct {
 	index  int
 	record ringbuf.Record
 }
 
 type dataBlock struct {
-	data [4096]uint8
+	data [DataBlockSize]uint8
 	len  uint32
 }
 
-// type indexedDataBlock struct {
-// 	index int
-// 	data  dataBlock
-// }
+type indexedDataBlock struct {
+	index int
+	data  dataBlock
+}
+
+func makeIndexedDataBlock(index int, data [DataBlockSize]uint8, dataLen uint32) indexedDataBlock {
+	return indexedDataBlock{
+		index: index,
+		data:  dataBlock{data: data, len: dataLen},
+	}
+}
