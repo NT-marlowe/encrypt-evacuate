789e85a214b6fd1f7bb2f6b8fb7f0e8dfe784f13
NT-marlowe <n.tezuka196.ninj@gmail.com>
2024-10-07 14:48:35 +0900

    algo

--- internal/priority_queue/sort_chan.go
+++ internal/priority_queue/sort_chan.go
@@ -1,5 +1,9 @@
 package priority_queue
 
+import (
+	"container/heap"
+)
+
 func RestoreOrder(reorderedChan <-chan Item) <-chan Item {
 	sortedChan := make(chan Item)
 
@@ -7,16 +11,59 @@ func RestoreOrder(reorderedChan <-chan Item) <-chan Item {
 		defer close(sortedChan)
 
 		for {
-			item, ok := <-reorderedChan
+			tmpItem, ok := <-reorderedChan
 			if !ok {
 				return
 			}
 
 			// do some sorting
 
-			sortedChan <- item
+			sortedChan <- tmpItem
 		}
 	}()
 
 	return sortedChan
+}
+
+func minHeapSort(inputChan <-chan Item) <-chan Item {
+	outputChan := make(chan Item)
+
+	go func() {
+		defer close(outputChan)
+		currentMinIndex := 0
+
+		pq := make(PriorityQueue, 0)
+		heap.Init(&pq)
+
+		for {
+			tmpItem, ok := <-inputChan
+			if !ok {
+				return
+			}
+
+			if tmpItem.index == currentMinIndex {
+				outputChan <- tmpItem
+				currentMinIndex++
+				continue
+			}
+
+			if pq.Len() == 0 {
+				heap.Push(&pq, &tmpItem)
+				continue
+			}
+
+			minItem := pq.Pop().(*Item)
+			if minItem.index == currentMinIndex {
+				outputChan <- *minItem
+				currentMinIndex++
+			} else {
+				heap.Push(&pq, minItem)
+			}
+
+			heap.Push(&pq, &tmpItem)
+		}
+
+	}()
+
+	return outputChan
 }
