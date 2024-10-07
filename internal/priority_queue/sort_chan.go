cc05c373732fd542a6d2c4b74d252c503a1e44f3
NT-marlowe <n.tezuka196.ninj@gmail.com>
2024-10-07 11:36:07 +0900

    function template

--- internal/priority_queue/sort_chan.go
+++ internal/priority_queue/sort_chan.go
@@ -1,5 +1,22 @@
 package priority_queue
 
-func tmp() <-chan Item {
-	return nil
+func tmp(reorderedChan <-chan Item) <-chan Item {
+	sortedChan := make(chan Item)
+
+	go func() {
+		defer close(sortedChan)
+
+		for {
+			item, ok := <-reorderedChan
+			if !ok {
+				return
+			}
+
+			// do some sorting
+
+			sortedChan <- item
+		}
+	}()
+
+	return sortedChan
 }
