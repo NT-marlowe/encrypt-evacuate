18ac98da58c35465e88b89d6bc2472a0bc31f82f
Naoya Tezuka <49357958+NT-marlowe@users.noreply.github.com>
2024-10-07 17:11:43 +0900

    Pq sort (#11)
    * .
    
    * function template
    
    * .
    
    * algo
    
    * rename
    
    * bug arimasu
    
    * tmp
    
    * closure is the cause
    
    * fix wrong index update
    
    * ok
    
    * test pass!
    
    * parallel write ok
    
    * ok
    
    * .
    
    * .

--- internal/priority_queue/pq.go
+++ internal/priority_queue/pq.go
@@ -9,12 +9,25 @@ type Item struct {
 	value any
 }
 
+func MakeItem(index int, value any) Item {
+	return Item{index: index, value: value}
+}
+
+func (item *Item) GetValue() any {
+	return item.value
+}
+
+func (item *Item) GetIndex() int {
+	return item.index
+}
+
 type PriorityQueue []*Item
 
 func (pq PriorityQueue) Len() int {
 	return len(pq)
 }
 
+// Min-heap.
 func (pq PriorityQueue) Less(i, j int) bool {
 	return pq[i].index < pq[j].index
 }
@@ -23,20 +36,23 @@ func (pq PriorityQueue) Swap(i, j int) {
 	pq[i], pq[j] = pq[j], pq[i]
 }
 
+// Min-heap.
+// |index| should not be updated because it is used to track the original position of the item.
 func (pq *PriorityQueue) Push(x any) {
-	n := len(*pq)
+	// n := len(*pq)
 	item := x.(*Item)
-	item.index = n
+	// item.index = n
 	*pq = append(*pq, item)
 }
 
+// Min-heap.
 func (pq *PriorityQueue) Pop() any {
 	old := *pq
 	n := len(old)
-	item := old[n-1]
-	old[n-1] = nil
+	item := old[0]
+	old[0] = nil
 
-	*pq = old[0 : n-1]
+	*pq = old[1:n]
 	return item
 }
 
