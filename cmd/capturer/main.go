936428492130fb737f0c985212ef3c13a345221c
NT-marlowe <n.tezuka196.ninj@gmail.com>
2024-10-13 14:46:19 +0900

    remove old

--- cmd/capturer/main.go
+++ cmd/capturer/main.go
@@ -45,7 +45,7 @@ func main() {
 
 	// Load the compiled eBPF ELF and load it into the kernel.
 	var objs capture_plainObjects
-	if err := loadCapture_sslObjects(&objs, nil); err != nil {
+	if err := loadCapture_plainObjects(&objs, nil); err != nil {
 		log.Fatal("Loading eBPF objects:", err)
 	}
 	defer objs.Close()
