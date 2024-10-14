e32939bacf6343edb010311fccfa0d85ee742668
NT-marlowe <n.tezuka196.ninj@gmail.com>
2024-10-14 20:46:39 +0900

    rm

--- ebpf_src/capture_plain.c
+++ ebpf_src/capture_plain.c
@@ -5,35 +5,9 @@
 #include <bpf/bpf_tracing.h>
 #include <linux/ptrace.h>
 
-#include "vmlinux_subset.h"
 #include "helpers.h"
 #include "constants.h"
 #include "maps.h"
-
-// struct enc_data_event_t {
-// 	unsigned char data[MAX_DATA_LEN];
-// 	int data_len;
-// };
-// struct enc_data_event_t *unused __attribute__((unused));
-
-// struct {
-// 	__uint(type, BPF_MAP_TYPE_HASH);
-// 	__uint(max_entries, 1024);
-// 	__type(key, uintptr_t);
-// 	__type(value, int);
-// } ptr_to_fd SEC(".maps");
-
-// struct {
-// 	__uint(type, BPF_MAP_TYPE_HASH);
-// 	__uint(max_entries, 1024);
-// 	__type(key, long);
-// 	__type(value, char[MAX_FILENAME_LEN]);
-// } fd_to_filename SEC(".maps");
-
-// struct {
-// 	__uint(type, BPF_MAP_TYPE_RINGBUF);
-// 	__uint(max_entries, 1024 * 1024);
-// } events_ringbuf SEC(".maps");
 
 SEC("uprobe/lib/x86_64-linux-gnu/"
 	"libcrypto.so.3:EVP_"
