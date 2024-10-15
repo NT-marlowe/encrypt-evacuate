be6c29626ef3cc8597b95194bef70252d2a4329d
Naoya Tezuka <49357958+NT-marlowe@users.noreply.github.com>
2024-10-15 10:01:14 +0900

    add map (#20)

---
+++ ebpf_src/maps.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <bpf/bpf_helpers.h>
+#include <linux/bpf.h>
+
+#include "constants.h"
+#include "vmlinux_subset.h"
+
+struct enc_data_event_t {
+    unsigned char data[MAX_DATA_LEN];
+    int data_len;
+};
+struct enc_data_event_t *unused __attribute__((unused));
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 1024);
+    __type(key, uintptr_t);
+    __type(value, int);
+} ptr_to_fd SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 1024);
+    __type(key, long);
+    __type(value, char[MAX_FILENAME_LEN]);
+} fd_to_filename SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_RINGBUF);
+    __uint(max_entries, 1024 * 1024);
+} events_ringbuf SEC(".maps");
