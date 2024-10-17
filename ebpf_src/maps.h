cccccb7a291e1dc1b950ec1ae1f3775b12f90aac
NT-marlowe <n.tezuka196.ninj@gmail.com>
2024-10-17 13:39:19 +0900

    ok

--- ebpf_src/maps.h
+++ ebpf_src/maps.h
@@ -7,26 +7,26 @@
 #include "vmlinux_subset.h"
 
 struct enc_data_event_t {
-    unsigned char data[MAX_DATA_LEN];
-    int data_len;
+	unsigned char data[MAX_DATA_LEN];
+	int data_len;
 };
 struct enc_data_event_t *unused __attribute__((unused));
 
 struct {
-    __uint(type, BPF_MAP_TYPE_HASH);
-    __uint(max_entries, 1024);
-    __type(key, uintptr_t);
-    __type(value, int);
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 1024);
+	__type(key, uintptr_t);
+	__type(value, int);
 } ptr_to_fd SEC(".maps");
 
 struct {
-    __uint(type, BPF_MAP_TYPE_HASH);
-    __uint(max_entries, 1024);
-    __type(key, long);
-    __type(value, char[MAX_FILENAME_LEN]);
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 1024);
+	__type(key, long);
+	__type(value, char[MAX_FILENAME_LEN]);
 } fd_to_filename SEC(".maps");
 
 struct {
-    __uint(type, BPF_MAP_TYPE_RINGBUF);
-    __uint(max_entries, 1024 * 1024);
+	__uint(type, BPF_MAP_TYPE_RINGBUF);
+	__uint(max_entries, 8 * 1024 * 1024);
 } events_ringbuf SEC(".maps");
