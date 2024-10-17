0946043e6cb48fedb3fca131e0837d3730ff03ca
NT-marlowe <n.tezuka196.ninj@gmail.com>
2024-10-17 15:48:46 +0900

    ok

--- internal/priority_queue/restore_order_test.go
+++ internal/priority_queue/restore_order_test.go
@@ -9,9 +9,9 @@ import (
 
 func TestRestoreOrder(t *testing.T) {
 	values := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	// values := []int{0, 9, 4, 5, 6, 3, 8, 1, 7, 2}
 
-	inputCh := make(chan Item, len(values))
+	// inputCh := make(chan Item, len(values))
+	inputCh := make(chan Item)
 
 	// reorder the values
 	for _, value := range values {
