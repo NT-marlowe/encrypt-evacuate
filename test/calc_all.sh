b7e6d551487d0ac654052a1fc8a1a268a6e8ba37
NT-marlowe <n.tezuka196.ninj@gmail.com>
2024-11-04 19:02:10 +0900

    update

--- test/calc_all.sh
+++ test/calc_all.sh
@@ -6,8 +6,9 @@ set -u
 # base_num=2
 subcom=$1
 
-power_2=(2 4 8 16 32)
-for i in ${power_2[@]}; do
-    echo "Running with ${i}MiB ring buffer"
-    ./calc_reten_match.sh ${subcom} $i > ./result/buf_capability/${subcom}_${i}MiB.csv
+power_2=(2 4 8 16 32 64)
+for p2 in ${power_2[@]}; do
+    echo "Running with ${p2}MiB ring buffer"
+    p2_padded=$(printf "%02d" $p2)
+    ./calc_reten_match.sh ${subcom} ${p2_padded} | tee ./result/buf_capability/${p2_padded}MiB_${subcom}.csv
 done
