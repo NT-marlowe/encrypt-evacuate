0f068910ee9ff44f575042f09e462f63a5f59af3
Naoya Tezuka <49357958+NT-marlowe@users.noreply.github.com>
2024-11-14 13:49:01 +0900

    Read pwd (#37)
    pwd is not embedded within shletered file paths.

--- ebpf_src/capture_plain.c
+++ ebpf_src/capture_plain.c
@@ -7,6 +7,7 @@
 
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
 // #include <linux/bpf.h>
 // #include <linux/ptrace.h>
 
@@ -45,6 +46,11 @@ int probe_entry_EVP_EncryptUpdate(struct pt_regs *ctx) {
 		bpf_printk("fd %d not found in fd_to_filename map\n", fd);
 		return 0;
 	}
+	const char *pwd = bpf_map_lookup_elem(&fd_to_pwd, &FD);
+	if (pwd == NULL) {
+		bpf_printk("fd %d not found in fd_to_filename map\n", fd);
+		return 0;
+	}
 
 	struct enc_data_event_t *event;
 	event = bpf_ringbuf_reserve(&events_ringbuf, sizeof(*event), 0);
@@ -59,6 +65,7 @@ int probe_entry_EVP_EncryptUpdate(struct pt_regs *ctx) {
 	bpf_probe_read_user(event->data, event->data_len, plaintext_buf);
 
 	bpf_probe_read_kernel_str(event->filename, MAX_FILENAME_LEN, filename);
+	bpf_probe_read_kernel_str(event->pwd, MAX_PATH_LEN, pwd);
 	event->offset = offset->prev_offset;
 
 	bpf_ringbuf_submit(event, 0);
@@ -135,8 +142,75 @@ int BPF_PROG(fexit_do_sys_open, const int dfd, const char *filename,
 		return 0;
 	}
 
+	struct task_struct *task = (struct task_struct *)bpf_get_current_task();
+	if (task == NULL) {
+		return 0;
+	}
+
+	struct dentry *pwd_dentry;
+	int err = BPF_CORE_READ_INTO(&pwd_dentry, task, fs, pwd.dentry);
+	if (err) {
+		bpf_printk("Failed to read task->fs->pwd\n");
+		return 0;
+	}
+
+	// Read the path of pwd so that the absolute path of the file can be
+	// obtained.
+	char path_buf[MAX_PATH_LEN];
+	u16 length            = 0;
+	struct dentry *parent = NULL;
+	for (int i = 0; i < MAX_LOOP; i++) {
+		const unsigned char *dirname = BPF_CORE_READ(pwd_dentry, d_name.name);
+		if (length < MAX_PATH_LEN - DNAME_LEN - 1) {
+			int tmp_len = bpf_probe_read_kernel_str(
+				path_buf + length, DNAME_LEN, dirname);
+			if (tmp_len > 0) {
+				length += tmp_len;
+				// bitwise-AND convinces the verifier that this memory access is
+				// safe.
+				// https://stackoverflow.com/questions/78525670/ebpf-verifier-error-unbounded-variable-offset-read-when-read-is-safe-and-withi
+				path_buf[(length - 1) & (MAX_PATH_LEN - 1)] = '/';
+			}
+
+			parent = BPF_CORE_READ(pwd_dentry, d_parent);
+			if (parent == pwd_dentry) {
+				break;
+			}
+			pwd_dentry = parent;
+		}
+	}
+	if (bpf_map_update_elem(&fd_to_pwd, &ret, &path_buf, BPF_ANY) != 0) {
+		bpf_printk("Failed to update fd_to_pwd map\n");
+		return 0;
+	}
+
+	// bpf_printk("path: %s\n", path_buf);
 	return 0;
 }
+
+// SEC("kretprobe/do_sys_openat2")
+// int BPF_KRETPROBE(kretprobe_openat2, long ret) {
+// 	// const int fd = ret;
+// 	// bpf_printk("kretprobe_openat2: %d\n", fd);
+
+// 	struct task_struct *task = (struct task_struct *)bpf_get_current_task();
+// 	if (task == NULL) {
+// 		return 0;
+// 	}
+
+// 	struct dentry *pwd_dentry;
+// 	int err = BPF_CORE_READ_INTO(&pwd_dentry, task, fs, pwd.dentry);
+// 	if (err) {
+// 		bpf_printk("Failed to read task->fs->pwd\n");
+// 		return 0;
+// 	}
+
+// 	char path[128];
+// 	bpf_d_path(pwd_dentry, path, sizeof(path));
+// 	bpf_printk("path: %s\n", path);
+
+// 	return 0;
+// }
 
 SEC("fexit/ksys_lseek")
 int BPF_PROG(fexit_ksys_lseek, unsigned int fd, long offset,
@@ -152,7 +226,6 @@ int BPF_PROG(fexit_ksys_lseek, unsigned int fd, long offset,
 
 	offset_entry->is_seeked     = 1;
 	offset_entry->seeked_offset = ret;
-	// bpf_printk("offset is updated to %ld\n", ret);
 
 	return 0;
 }
