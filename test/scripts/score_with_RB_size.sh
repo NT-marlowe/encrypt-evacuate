77f37654133cebc3fc032d6a8505ef9ad8f05362
NT-marlowe <n.tezuka196.ninj@gmail.com>
2024-11-16 19:13:05 +0900

    .

--- test/scripts/score_with_RB_size.sh
+++ test/scripts/score_with_RB_size.sh
@@ -9,19 +9,26 @@ function main() {
 
     check_root
 
-    local parallelism=$1
-    local RB_size=$2
-
-    # ${current_dir}/experiment_altogether.sh ${parallelism}
-
-    padded_p=$(printf "%02d" ${parallelism})
-
+    local map_file=${current_dir}/../../ebpf_src/maps.h
     local result_dir=${current_dir}/../result/buf_capability/journal
+
+    local parallelism=$1
+    local padded_p=$(printf "%02d" ${parallelism})
+
+    local RB_sizes=(2 4 8 16 32)
+    for size in ${RB_sizes[@]}; do
+        echo "RB size: ${size} MiB"
+        sed -i "s/^#define MULT .*$/#define MULT $((size))/" ${map_file}
+
+        ${current_dir}/experiment_altogether.sh ${parallelism}
+        ${current_dir}/../calc_reten_match.sh match >${result_dir}/match_${padded_p}_${size}MiB.csv
+    done
+
     # ${current_dir}/../calc_reten_match.sh reten >${result_dir}/reten_${padded_p}.csv
-    ${current_dir}/../calc_reten_match.sh match >${result_dir}/match_${padded_p}_${RB_size}MiB.csv
 
     sleep 2
 
+    sed -i "s/^#define MULT .*$/#define MULT 1/" ${map_file}
     set_owner ${result_dir}
 }
 
--- test/vizualize/plot.py
+++ test/vizualize/plot.py
@@ -1,18 +1,19 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 import os
 import re
 
 
 def parse_name(filename: str):
-    pattern = re.compile(r"(\d+)")
+    pattern = re.compile(r"(\d+)MiB")
     match = re.search(pattern, filename)
     if match:
         return int(match.group(1))
 
 
 # ディレクトリとファイルの設定
-directory = "../result/buf_capability"
+directory = "../result/buf_capability/journal"
 files = os.listdir(directory)
 files = sorted(files, key=lambda x: parse_name(x))
 
@@ -24,28 +25,32 @@ for file in files:
     if file.startswith("match"):
         # print(file)
         filepath = os.path.join(directory, file)
-        data = pd.read_csv(filepath, header=None, names=["filesize", "rate"])
+        data = pd.read_csv(filepath, header=None, names=["filesize", "rate"])[1:]
         # ファイルサイズをMB単位で表示できるように処理
         data["filesize"] = (
             data["filesize"].str.extract(r"\d+_(\d+)MB\.data").astype(int)
         )
+        data["rate"] = data["rate"].astype(float)
 
         plt.plot(
             data["filesize"], data["rate"], marker="o", label=f"{parse_name(file)} MiB"
         )
+        print(data["rate"])
 
 # グラフの設定
 plt.xlabel("Size of Original File [MB]", fontsize=16)
 plt.xscale("log", base=2)
 plt.xticks([2**x for x in range(8)], [str(2**x) for x in range(8)])
 plt.ylabel("Rate", fontsize=16)
+plt.yticks(np.arange(0, 1.1, 0.2))
 plt.title(
     "Valiation of Match Rate with File Size for Different Ring Buffer Sizes",
     fontsize=16,
 )
+
 plt.legend(title="Ring Buffer Size", fontsize=10)
 plt.grid(True)
 plt.tight_layout()
 
 # グラフを表示
-plt.savefig("./img/buf_capability.png")
+plt.savefig("./img/buf_capability_seek.png")
