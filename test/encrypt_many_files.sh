4a499f2160c6bb423f9e7736df6ad30f1fd55420
NT-marlowe <n.tezuka196.ninj@gmail.com>
2024-11-17 12:28:08 +0900

    upate data

--- test/encrypt_many_files.sh
+++ test/encrypt_many_files.sh
@@ -2,8 +2,12 @@
 #
 set -eu
 out_filename=$1
+time_len=30
 
-iostat -xyt -o JSON 1 15 > ${out_filename} &
+sudo sync
+echo 3 | sudo tee /proc/sys/vm/drop_caches
+
+iostat -xyt -o JSON 1 ${time_len} > ${out_filename} &
 
 for file in $(ls ./files_sysbench | grep -v 'enc'); do
     echo "Encrypting ${file}"
