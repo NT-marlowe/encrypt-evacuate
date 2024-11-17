37ad531b21e83aea7d1a2015c2ab25c8731def0f
NT-marlowe <n.tezuka196.ninj@gmail.com>
2024-11-17 11:53:25 +0900

    .

--- test/vizualize/io_cpu_plot.py
+++ test/vizualize/io_cpu_plot.py
@@ -3,16 +3,12 @@ import matplotlib.pyplot as plt
 import sys
 
 
-def visualize_cpu_usage(json_file):
-    # JSONファイルを読み込む
-    with open(json_file, "r") as file:
-        data = json.load(file)
-
+def plot(iostat_data, label: str):
     # データからタイムスタンプとCPU使用率を抽出
     xticks = [i for i in range(1, 15 + 1, 1)]
     cpu_usage = []
 
-    for stat in data["sysstat"]["hosts"][0]["statistics"]:
+    for stat in iostat_data["sysstat"]["hosts"][0]["statistics"]:
         # timestamps.append(stat["timestamp"])
         avg_cpu = stat["avg-cpu"]
         cpu_usage.append(avg_cpu["user"] + avg_cpu["system"])
@@ -23,8 +19,20 @@ def visualize_cpu_usage(json_file):
         xticks,
         cpu_usage,
         marker="o",
-        label="CPU Usage (User + System)",
+        label=label,
     )
+    plt.xticks()
+    plt.tight_layout()
+
+
+def visualize_cpu_usage(json_file):
+    # JSONファイルを読み込む
+    with open(json_file, "r") as file:
+        data = json.load(file)
+
+    # データからタイムスタンプとCPU使用率を抽出
+    plot(data, "CPU Usage")
+
     plt.xticks(rotation=45)
     plt.xlabel("Timestamp")
     plt.ylabel("CPU Usage (%)")
