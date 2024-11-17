35279ba6f31aae074a46d475f2ea9881a81dc210
NT-marlowe <n.tezuka196.ninj@gmail.com>
2024-11-17 18:11:30 +0900

    exp 5 times

--- test/encrypt_many_files.sh
+++ test/encrypt_many_files.sh
@@ -2,17 +2,34 @@
 #
 set -eu
 out_filename=$1
+iter=$2
 time_len=30
 
-sudo sync
-echo 3 | sudo tee /proc/sys/vm/drop_caches
+function run_once() {
+    local filename=$1
+    echo $filename
 
-iostat -xyt -o JSON 1 ${time_len} > ${out_filename} &
+	sudo sync
+	echo 3 | sudo tee /proc/sys/vm/drop_caches
 
-for file in $(ls ./files_sysbench | grep -v 'enc'); do
-    echo "Encrypting ${file}"
-    ./my_simple_ransomware ./files_sysbench/${file}
-done
+	iostat -xyt -o JSON 1 ${time_len} > ${filename} &
+	pid=$!
 
-echo "Cleaning up..."
-rm -rf ./files_sysbench/*.enc
+	for file in $(ls ./files_sysbench | grep -v 'enc'); do
+	    echo "Encrypting ${file}"
+	    ./my_simple_ransomware ./files_sysbench/${file}
+	done
+
+	echo "Cleaning up..."
+	rm -rf ./files_sysbench/*.enc
+	wait $pid
+}
+
+function main() {
+	for i in `seq 1 ${iter}`; do
+		run_once ${out_filename}.${i}
+	done
+}
+
+main "$@"
+	
