8cdd8b3b9cb8b05e619dc9363598e7b04bbf4687
NT-marlowe <n.tezuka196.ninj@gmail.com>
2024-11-20 14:19:53 +0900

    pid=#! is changed

--- test/encrypt_many_files.sh
+++ test/encrypt_many_files.sh
@@ -4,7 +4,7 @@ set -eu
 out_filename=$1
 iter=$2
 
-time_len=30
+time_len=10
 EBPF_PROGRAM=ebpf-ssl
 
 function run_once() {
@@ -35,23 +35,40 @@ function build() {
 }
 
 function main() {
+    local parallelism
     if [ "$#" -eq 3 ]; then
+        if [ "$EUID" -ne 0 ]; then
+            echo "Please run as root"
+            exit
+        fi
+
         echo "Building..."
         parallelism=$3
         build
     fi
 
     for i in $(seq 1 ${iter}); do
+        echo $i
+        local pid
+
         if [ "$#" -eq 3 ]; then
-            ../${EBPF_PROGRAM} ${parallelism} &
+            echo $parallelism
+            ../${EBPF_PROGRAM} ${parallelism} >stdout.log 2>stderr.log &
             pid=$!
+            copied_pid=${pid}
+            echo "pid is ${pid}"
+            # search process with $pid
+            ps auxww | grep ${pid} | grep -v grep
+            sleep 1
         fi
         run_once ${out_filename}.${i}
 
         sleep 2
 
         if [ "$#" -eq 3 ]; then
-            kill -SIGINT ${pid} >/dev/null 2>&1
+            echo ${pid}
+            # kill -SIGINT ${pid} >/dev/null 2>&1
+            kill -SIGINT ${copied_pid}
             sleep 2
         fi
     done
